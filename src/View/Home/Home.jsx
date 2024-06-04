f99fc24692bf3941b1b9840466211d5dc6ae686c
ravib880 <ravi.bundheliya@sridixtechnology.com>
2024-06-04 14:56:01 +0530

    header added

--- src/View/Home/Home.jsx
+++ src/View/Home/Home.jsx
@@ -8,23 +8,35 @@ function Home() {
     <div className='main_home'>
       <Carousel fade>
         <Carousel.Item>
-          <Link to='/bridal'>
+          <Link className='d-none d-md-block' to='/bridal'>
             <img src={require('../../Assets/Img/sld11.png')} alt="" />
           </Link>
+          <Link className='d-block d-md-none' to='/bridal'>
+            <img src={require('../../Assets/Img/sld1.png')} alt="" />
+          </Link>
         </Carousel.Item>
         <Carousel.Item>
-        <Link to='/bridal'>
+          <Link className='d-none d-md-block' to='/bridal'>
             <img src={require('../../Assets/Img/sld12.png')} alt="" />
           </Link>
-        </Carousel.Item>
-        <Carousel.Item>
-        <Link to='/bridal'>
-            <img src={require('../../Assets/Img/sld13.png')} alt="" />
+          <Link className='d-block d-md-none' to='/bridal'>
+            <img src={require('../../Assets/Img/sld2.png')} alt="" />
           </Link>
         </Carousel.Item>
         <Carousel.Item>
-        <Link to='/bridal'>
+          <Link className='d-none d-md-block' to='/bridal'>
+            <img src={require('../../Assets/Img/sld13.png')} alt="" />
+          </Link>
+          <Link className='d-block d-md-none' to='/bridal'>
+            <img src={require('../../Assets/Img/sld3.png')} alt="" />
+          </Link>
+        </Carousel.Item>
+        <Carousel.Item>
+          <Link className='d-none d-md-block' to='/bridal'>
             <img src={require('../../Assets/Img/sld14.png')} alt="" />
+          </Link>
+          <Link className='d-block d-md-none' to='/bridal'>
+            <img src={require('../../Assets/Img/sld4.png')} alt="" />
           </Link>
         </Carousel.Item>
       </Carousel>
